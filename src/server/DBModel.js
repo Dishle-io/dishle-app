2dfca1203efbea44f4b5bf4d4f87c33ceffbdaa7
James Ma <james.shny1721@gmail.com>
2022-04-19 14:18:27 -0700

    add random meal generator

--- src/server/DBModel.js
+++ src/server/DBModel.js
@@ -5,11 +5,10 @@ const mongoose = require('mongoose');
 const recipeSchema = new mongoose.Schema({
   mealID: {type: String, required: true},
   mealName: {type: String, required: true},
-  mealCategory: {type: String, required: true},
-  mealEthnicity: {type: String, required: true},
-  mealInstructions: {type: String, required: true},
-  mealThumbnail: {type: String, required: true},
-  mealIngredients: {type: [String], required: true}
+  cuisine: {type: String},
+  mealInstructions: {type: String},
+  mealThumbnail: {type: String},
+  mealIngredients: {type: [String]}
 });
 
 module.exports = mongoose.model('recipe', recipeSchema);
