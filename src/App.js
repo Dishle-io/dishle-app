de99a896cd493c11aefd7aace3ba1de8ebdd8476
DaneCorpion <danecorpion@gmail.com>
2022-04-19 11:32:20 -0400

    top-component2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import { TopComponents } from './TopComponents';
 
 function App() {
+  
+  const [dish, setDish] = useState([])
+  const [cuisine, setCuisine] = useState([])
+
+  useEffect(() => {
+    fetch("/get")
+      .then((response) => response.json()) 
+      .then((data) => setDish(data.dish))
+      .then((data) => setCuisine(data.cuisine));
+  }, [])
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <TopComponents />
         <Counter />
         <p>
           Edit <code>src/App.js</code> and save to reload.
