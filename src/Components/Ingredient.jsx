080d2e26e51218938c874ff58431155b631db313
James Ma <james.shny1721@gmail.com>
2022-04-20 10:24:46 -0700

    updated

--- src/Components/Ingredient.jsx
+++ src/Components/Ingredient.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import React from 'react'
 import { ListItemText } from '@mui/material'
 export default function Ingredient(props) {
   const { name } = props;
@@ -9,7 +9,7 @@ export default function Ingredient(props) {
   //   bgcolor: 'grey',
   //   padding: 2
   // };
-  
+
 
   return (
     <ListItemText variant='outlined' primary={(isReveal ? name : "???")}></ListItemText>
